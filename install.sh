c1001b785e3b0490f69f9fdc26b207a021169717
Jan Knedlik <j.knedlik@gsi.de>
2018-12-05 19:11:26 +0100

    also autoload pathogen

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 
 # clone vundle into vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#also get pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 #install vundle plugins
 vim +PluginInstall +qall
