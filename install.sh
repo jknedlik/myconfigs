db94cc5f12acbcbb18c7edcc247519d9444623eb
Jan Knedlik <j.knedlik@gsi.de>
2018-12-05 19:27:13 +0100

    use softlink for easy change of vim stuff

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #also get pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+ln -s $PWD/vimrc ~/.vimrc
 
 #install vundle plugins
 vim +PluginInstall +qall
