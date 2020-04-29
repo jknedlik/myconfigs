b93b8b07c2c11c2fbfae9cd6eab653d422092a22
Jan Knedlik <j.knedlik@gsi.de>
2020-04-29 20:02:55 +0200

    added powerline10k

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/.zshrc ~/.zshrc
+ln -s $PWD/p10k.zsh ~/.p10k.zsh
 
 #install vundle plugins
 vim +PluginInstall +qall
