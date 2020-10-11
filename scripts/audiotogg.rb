2e6cebc384010d9bdf6e958ec522d7a2bb6c6ec6
Jan Knedlik <j.knedlik@gsi.de>
2020-10-11 11:13:20 +0200

    Better audio output and using mako/notify-send

--- scripts/audiotogg.rb
+++ scripts/audiotogg.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
 require 'json'
-s=File.read("#{`hostname`.split(" ")[0]}-sinks.json")
-list=[]
-JSON.parse(s)['sinks'].each do |sink| 
-  list.append(`pactl list short sinks |grep #{sink}`.split(" ")[0])
+HASH={}
+def nextk(key)
+  return HASH.flatten[(HASH.flatten.index(key)+2)%HASH.flatten.length]
 end
-
+s=File.read("#{`hostname`.split(" ")[0]}-sinks.json")
+JSON.parse(s)['sinks'].each do |sink| 
+  HASH[`pactl list short sinks |grep #{sink[0]}`.split(" ")[0]]=sink[1]
+  HASH[`pactl list short sinks |grep #{sink[0]}`.split(" ")[0]]=sink[1]
+end
+puts("list:\" #{HASH} \"")
 sinkInputs=`pactl list short sink-inputs`.split("\n")
 currentSink=sinkInputs[0].split(" ")[1]
-  Process.spawn("pactl set-default-sink #{list[(list.index(currentSink)+1)%list.length]}")
+puts("current sink: #{currentSink} next: #{nextk currentSink} flat: #{HASH.flatten[0]}")
+target=(HASH.keys.include? currentSink)? nextk(currentSink) : HASH.flatten[0]
+puts("move currentSink and default target to #{target}")
+Process.spawn("notify-send -t 3000 \"switch 🎶\n #{HASH[target]}\"")
+Process.spawn("pactl set-default-sink #{target}")
 for x in sinkInputs do
-  puts( x, currentSink)
-  Process.spawn("pactl move-sink-input #{x.split(" ")[0]} #{list[(list.index(currentSink)+1)%list.length]}")
+  Process.spawn("pactl move-sink-input #{x.split(" ")[0]} #{target}")
 end
