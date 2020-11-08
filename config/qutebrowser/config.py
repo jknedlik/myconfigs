67256e0ef0d44e70a148782dbaf18455433d634a
Jan Knedlik <j.knedlik@gsi.de>
2020-11-08 23:29:46 +0100

    Removed unused searchengines, config via json

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,31 +1,19 @@
 c.backend = 'webengine'
 c.fonts.default_size = "20px"
-config.bind(',p', 'spawn --userscript qute-pass --dmenu-invocation dmenu')
-config.bind(',P', 'spawn --userscript qute-pass --dmenu-invocation dmenu --password-only')
-config.bind('<Ctrl-Shift-y>','hint links spawn --detach mpv --force-window yes {hint-url}')
-config.bind(",n", "config-cycle content.user_stylesheets /home/jknedlik/.config/qutebrowser/solarized-all-sites-dark.css \"\"")
-config.bind(';m', 'hint links spawn mpv {hint-url}')
+c.bindings.commands["normal"]={
+',p': 'spawn --userscript qute-pass --dmenu-invocation dmenu',
+',P': 'spawn --userscript qute-pass --dmenu-invocation dmenu --password-only',
+'<Ctrl-Shift-y>':'hint links spawn --detach mpv --force-window yes {hint-url}',
+';m': 'hint links spawn mpv {hint-url}'}
 c.url.searchengines = {
     'DEFAULT':  'https://google.com/search?hl=en&q={}',
-    '!a':       'https://www.amazon.com/s?k={}',
-    '!d':       'https://duckduckgo.com/?ia=web&q={}',
-    '!dd':      'https://thefreedictionary.com/{}',
-    '!e':       'https://www.ebay.com/sch/i.html?_nkw={}',
-    '!fb':      'https://www.facebook.com/s.php?q={}',
+    '!a':       'https://www.amazon.de/s?k={}',
     '!gh':      'https://github.com/search?o=desc&q={}&s=stars',
     '!gist':    'https://gist.github.com/search?q={}',
-    '!gi':      'https://www.google.com/search?tbm=isch&q={}&tbs=imgo:1',
-    '!gn':      'https://news.google.com/search?q={}',
-    '!ig':      'https://www.instagram.com/explore/tags/{}',
     '!m':       'https://www.google.com/maps/search/{}',
-    '!p':       'https://pry.sh/{}',
     '!r':       'https://www.reddit.com/search?q={}',
-    '!sd':      'https://slickdeals.net/newsearch.php?q={}&searcharea=deals&searchin=first',
-    '!t':       'https://www.thesaurus.com/browse/{}',
-    '!tw':      'https://twitter.com/search?q={}',
-    '!w':       'https://en.wikipedia.org/wiki/{}',
-    '!yelp':    'https://www.yelp.com/search?find_desc={}',
-    '!yt':      'https://www.youtube.com/results?search_query={}'
+    '!wiki':       'https://en.wikipedia.org/wiki/{}',
+    '!y':      'https://www.youtube.com/results?search_query={}'
 }
 c.tabs.position = "right"
 c.colors.webpage.darkmode.enabled = True
