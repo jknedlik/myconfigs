d1ae1309638d58b5d2e98ee94f6f25c0ae14c9cd
Jan Knedlik <j.knedlik@gsi.de>
2020-11-14 13:45:51 +0100

    Rm install img from win10 mod script accordingly

--- scripts/startvm.sh
+++ scripts/startvm.sh
@@ -2,34 +2,30 @@
 
 HUGEPAGES_NR=8600
 oldnr=0
-
 sudo sh -c "echo $HUGEPAGES_NR >/proc/sys/vm/nr_hugepages"
 #try 10 times to allocate the memory for vm hugepages via memory pressure
-for i in $(seq 10)
+for i in $(seq 8)
 do
   sudo sh -c "echo $HUGEPAGES_NR >/proc/sys/vm/nr_hugepages"
   nr=$(cat /proc/sys/vm/nr_hugepages);
-  if [ $nr -lt $HUGEPAGES_NR ]
-  then
-    break
+  if [ $nr -ge $HUGEPAGES_NR ];
+  then # if done boot the vm
+	sudo virsh net-start default &
+	sudo chown root:kvm /dev/nvme0n1
+	sudo virsh start win10 
+	sudo systemctl stop usbserver.socket usbserver.service || true
+	sleep 0.5 
+	sudo systemctl start usbserver.socket usbserver.service || true
+	LIBASOUND_DEBUG=1 scream -o pulseaudio -i virbr0 -v -u -p 4011 &>/tmp/scream.log &
+	looking-glass-client spice:enable=no  -F -k 1>/tmp/looking-glass-log 2>/tmp/looking-glass-log & 
+	sudo systemctl start sshd
+	sudo swapusb.sh
+	exit
   fi
   sleep 1
 done
-if [ $nr -lt $HUGEPAGES_NR ]
-then
-sudo sh -c"echo $oldnr >/proc/sys/vm/nr_hugepages"
+sudo sh -c "echo 0 >/proc/sys/vm/nr_hugepages"
 echo "could not get enough hugepages"
 exit
-fi
 
-sudo virsh net-start default
-sudo chown root:kvm /dev/nvme0n1
-sudo virsh start win10
-sudo systemctl stop usbserver.socket usbserver.service || true
-sleep 1
-sudo systemctl start usbserver.socket usbserver.service || true
-LIBASOUND_DEBUG=1 scream -o pulseaudio -i virbr0 -v -u -p 4011 &>/tmp/scream.log &
-looking-glass-client spice:enable=no  -F -k 1>/tmp/looking-glass-log 2>/tmp/looking-glass-log & 
-sudo systemctl start sshd
-sudo swapusb.sh
 
