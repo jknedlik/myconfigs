d7e2f070b86fbbda27d0c42fe59e6933f9d127d9
Jan Knedlik <j.knedlik@gsi.de>
2021-02-28 11:31:59 +0100

    Added adblock filter usage for qtbrowser

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,3 +1,4 @@
+config.load_autoconfig()
 c.backend = 'webengine'
 c.fonts.default_size = "20px"
 c.bindings.commands["normal"]={
@@ -45,10 +46,19 @@ c.colors.tabs.selected.odd.fg="#f5870a"
 c.colors.tabs.selected.even.fg="#f5870a"
 #do not flash pages white before css is loaded
 c.colors.webpage.bg="#121212"
-
 c.colors.tabs.even.bg="#202020"
-
 c.colors.hints.bg="#121212"
 c.colors.hints.fg="#f5870a"
-
 c.colors.contextmenu.menu.bg="#121212"
+
+# adblock settings
+c.content.blocking.method="both"
+c.content.blocking.adblock.lists=[
+  "https://easylist.to/easylist/easylist.txt",
+  "https://easylist.to/easylist/easyprivacy.txt",
+  "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext",
+  "https://curben.gitlab.io/malware-filter/urlhaus-filter-online.txt",
+  "https://gitcdn.xyz/repo/NanoAdblocker/NanoFilters/master/NanoMirror/NanoDefender.txt",
+  "https://gitcdn.xyz/repo/NanoAdblocker/NanoFilters/master/NanoFilters/NanoBase.txt"
+   "https://easylist.to/easylistgermany/easylistgermany.txt"
+]
