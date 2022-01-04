059011bd272401b49df5651491f559685a69d441
Jan Knedlik <j.knedlik@gsi.de>
2022-01-04 22:55:31 +0100

    fix stow_all script

--- dotfiles/stow_all.sh
+++ dotfiles/stow_all.sh
@@ -1,1 +1,3 @@
- fd -t  d | xargs stow -t $HOME	
+ #fd -t  d | xargs stow -t $HOME	
+ #fd -t  d |  xargs -I {} sh -c "basename {} | stow -t $HOME"
+ fd -t  d |  xargs -I {} sh -c 'export stowmod=$(basename {}) && stow -t $HOME -S $stowmod'
