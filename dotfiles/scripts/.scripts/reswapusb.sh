93c71c1d04e5b7b33a521e149beec8e11b941be7
Jan Knedlik <j.knedlik@gsi.de>
2022-01-29 13:17:56 +0100

    Add new keyboard to switched devices

--- dotfiles/scripts/.scripts/reswapusb.sh
+++ dotfiles/scripts/.scripts/reswapusb.sh
@@ -2,3 +2,4 @@
 sudo echo 'win10 detach 04d9:0143' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
 sudo echo 'win10 detach 1e7d:2d60' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
 sudo echo 'win10 detach 1e7d:2d5f' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
+sudo echo 'win10 detach 1d50:6124' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
--- dotfiles/scripts/.scripts/swapusb.sh
+++ dotfiles/scripts/.scripts/swapusb.sh
@@ -3,6 +3,10 @@ sudo echo 'win10 detach 04d9:0143' | socat - UNIX-CONNECT:/var/run/vm-usbserver.
 sudo echo 'win10 detach 1e7d:2d60' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
 sudo echo 'win10 detach 1e7d:2d5f' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
 
+sudo echo 'win10 detach 1d50:6124' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
+
+
 sudo echo 'win10 attach 04d9:0143' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
 sudo echo 'win10 attach 1e7d:2d60' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
 sudo echo 'win10 attach 1e7d:2d5f' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
+sudo echo 'win10 attach 1d50:6124' | socat - UNIX-CONNECT:/var/run/vm-usbserver.sock
