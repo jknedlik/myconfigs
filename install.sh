6820b2b1c952a84aca58e50bf35f110fa8f0acd0
Jan Knedlik <j.knedlik@gsi.de>
2022-12-30 17:05:07 +0100

    remove all unneeded installs

--- install.sh
+++ install.sh
@@ -4,13 +4,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #also get pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-ln -s $PWD/vimrc ~/.vimrc
-ln -s $PWD/.zshrc ~/.zshrc
-ln -s $PWD/p10k.zsh ~/.p10k.zsh
 
 #install vundle plugins
 vim +PluginInstall +qall
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#instal antigen for zsh
-curl -L git.io/antigen > $HOME/.antigen.zsh
 
