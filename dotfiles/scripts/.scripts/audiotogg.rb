6265159104becd194e35a84220d26426820bb506
Jan Knedlik <j.knedlik@gsi.de>
2023-03-02 21:41:07 +0100

    Generalize script for inputs/outputs

--- dotfiles/scripts/.scripts/audiotogg.rb
+++ dotfiles/scripts/.scripts/audiotogg.rb
@@ -1,23 +1,27 @@
 #!/usr/bin/env ruby
 require 'json'
+
+cmd={"target" => "sink", "io"=>'input'}
+cmd={"target"=>"source", "io"=>'output'} if ARGV.length>0
 HASH={}
 def nextk(key)
   return HASH.flatten[(HASH.flatten.index(key)+2)%HASH.flatten.length]
 end
 s=File.read("#{`hostname`.split(" ")[0]}-sinks.json")
-JSON.parse(s)['sinks'].each do |sink| 
-  HASH[`pactl list short sinks |grep #{sink[0]}`.split(" ")[0]]=sink[1]
+
+JSON.parse(s)["#{cmd['target']}s"].each do |sink| 
+  HASH[`pactl list short #{cmd['target']}s |grep #{sink[0]}`.split(" ")[0]]=sink[1]
 end
 puts("list:\" #{HASH} \"")
-sinkInputs=`pactl list short sink-inputs`.split("\n")
+allTargets=`pactl list short #{cmd['target']}-#{cmd['io']}s`.split("\n")
 HASH.delete_if {|k,v| k==nil}
-defaultSinkName=`pactl info|grep "Default Sink"`.split()[2]
-currentSink=`pactl list short sinks |grep "#{defaultSinkName}"`.split(" ")[0]
-target=(HASH.keys.include? currentSink)? nextk(currentSink) : HASH.flatten[0]
-puts("current sink: #{currentSink} next: #{target}")
-puts("move currentSink and default target to #{target}")
+defaultName=`pactl info|grep -i "Default #{cmd['target']}"`.split()[2]
+current=`pactl list short #{cmd['target']}s |grep "#{defaultName}"`.split(" ")[0]
+target=(HASH.keys.include? current)? nextk(current) : HASH.flatten[0]
+puts("current #{cmd['target']}: #{current} next: #{target}")
+puts("move currentk and default target to #{target}")
 Process.spawn("notify-send -t 3000 \"switch 🎶 \n#{HASH[target]}\"")
-Process.spawn("pactl set-default-sink #{target}")
-for x in sinkInputs do
-  Process.spawn("pactl move-sink-input #{x.split(" ")[0]} #{target}")
+Process.spawn("pactl set-default-#{cmd['target']} #{target}")
+for x in allTargets do
+  Process.spawn("pactl move-#{cmd['target']}-#{cmd['io']} #{x.split(" ")[0]} #{target}")
 end
