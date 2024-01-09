8d6388206ae62d3b113fda120aeb6a47b9a2523b
Jan Knedlik <j.knedlik@gsi.de>
2024-01-09 15:48:10 +0100

    Go to next output if Output not found

--- dotfiles/scripts/.scripts/audiotogg.rb
+++ dotfiles/scripts/.scripts/audiotogg.rb
@@ -22,10 +22,16 @@ target, io              = ARGV[0]=="output"? ["sink","output"] : ["source","inpu
 ### calculate next device
 configured_devices      = parse_device_config("#{`hostname`.split(" ")[0]}-sinks.json")["#{io}"]
 default_target_name     = `pactl info|grep -i "Default #{target}"`.split()[2]
-next_device_name        = nextdevice(configured_devices,default_target_name)
-next_device_id          = `pactl list short #{target}s |grep "#{next_device_name}"`.split(" ")[0]
-puts("\ncurrent #{configured_devices[default_target_name]} -> next: #{configured_devices[next_device_name]}")
-puts("move all and default target(s) to#{next_device_id}")
+
+next_device_name        = default_target_name
+next_device_id          = nil
+while next_device_id.nil? do #empty if next_device_name not found
+  next_device_name        = nextdevice(configured_devices,next_device_name)
+  next_device_id          = `pactl list short #{target}s |grep "#{next_device_name}"`.split(" ")[0]
+end
+
+puts("\n#{configured_devices[default_target_name]} -> '#{configured_devices[next_device_name]}'")
+puts("move all and default target(s) to'#{next_device_id}''")
 
 ### spawn notification and set default target
 Process.spawn("notify-send -t 3000 \"🎶#{target=="output" ? "<-" : "->"} #{configured_devices[next_device_name]}\"")
